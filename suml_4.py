85d41fc2359686ea5160c638c87015164d6cb513
s20240 <101036696+bleble2137@users.noreply.github.com>
2023-05-10 18:09:27 +0200

    Update suml_4.py

--- suml_4.py
+++ suml_4.py
@@ -1,50 +1,36 @@
-import pickle
-import pathlib
 import streamlit as st
-from datetime import datetime
-
-startTime = datetime.now()
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+import pickle
 
 filename = 'model_.sv'
 model = pickle.load(open(filename, 'rb'))
-
-sex_d = {0: "Kobieta", 1: "Mężczyzna"}
-pclass_d = {0: "Pierwsza", 1: "Druga", 2: "Trzecia"}
-embarked_d = {0: "Cherbourg", 1: "Queenstown", 2: "Southampton"}
 
 
 def main():
     st.set_page_config(page_title="Serduszko nie puka w rytmie czaczy. S20240")
     overview = st.container()
-    left, right = st.columns(2)
+    right = st.container()
     prediction = st.container()
 
+
     st.image("https://i.kym-cdn.com/entries/icons/original/000/011/699/sad-broken-heart-l.png")
-
+    
     with overview:
-        st.title("Titanic")
-
-    with left:
-        sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
-        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2,
-                                  format_func=lambda x: embarked_d[x])
-        pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
-
+        st.title("Sercochoróbska predykcyja. S20240")
+    
     with right:
-        age_slider = st.slider("Wiek", value=1, min_value=1, max_value=90)
-        sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=10)
-        parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=10)
-        fare_slider = st.slider("Cena biletu", min_value=0, max_value=480, step=1)
-
-    data = [[pclass_radio, sex_radio, age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
+        var1 = st.slider("Wiek:", value=40, min_value=28, max_value=100)
+        var2 = st.slider("RBP", value=132, min_value=0, max_value=200)
+        var3 = st.slider("Cholesterol", value=200, min_value=60, max_value=610)
+        var4 = st.slider("Max Heart Rate", value=136, min_value=60, max_value=210)
+        var5 = st.slider("Old peak", value=0.8, min_value=-3.0, max_value=7.0, step=0.1)
+    
+    data = [[var1, var2, var3, var4, var5]]
     survival = model.predict(data)
     s_confidence = model.predict_proba(data)
-
+    
     with prediction:
-        st.subheader("Czy taka osoba przeżyłaby katastrofę?")
-        st.subheader(("Tak" if survival[0] == 1 else "Nie"))
+        st.subheader("Wynik")
+        st.subheader(("Chory" if survival[0] == 1 else "Zdrowy"))
         st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
 
 
